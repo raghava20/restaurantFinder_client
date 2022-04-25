07c8dacf9d369dc0483977cd4c964702bcd57e03
raghava20 <raghavaanand22@gmail.com>
2022-04-25 15:58:40 +0530

    aligned css

--- src/components/Reviews.tsx
+++ src/components/Reviews.tsx
@@ -3,11 +3,11 @@ import StarRating from "./StarRating";
 
 const Reviews = ({ review }: any) => {
   return (
-    <div className="flex flex-wrap justify-center sm:justify-start items-center mt-9">
+    <div className="flex flex-wrap justify-center sm:justify-center items-center mt-9">
       {review?.map((review: any) => {
         return (
           <div
-            className="flex text-white flex-col border-none bg-blue-500 mr-4 mb-4 gap-5 p-5 rounded-sm shadow-md shadow-blue-400 w-[270px]"
+            className="flex text-white flex-col border-none bg-blue-500 mr-4 mb-4 gap-5 p-5 rounded-sm shadow-md shadow-blue-400 w-[290px]"
             key={review.id}
           >
             <div className="flex flex-row justify-between items-center border-b-2 border-gray-400 pb-2">
